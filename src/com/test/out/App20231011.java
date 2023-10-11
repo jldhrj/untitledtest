a979915d7abef0121ddc5d5f8406584d0997b72f
brad <jldhrj@gmail.com>
2023-10-11 23:06:40 +0800

    test001

---
+++ src/com/test/out/App20231011.java
@@ -0,0 +1,10 @@
+package com.test.out;
+
+public class App20231011 {
+    public static void main(String[] args) {
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+
+}
--- src/com/test/out/app20231011.java
+++
@@ -1,7 +0,0 @@
-package com.test.out;
-
-public class app20231011 {
-    public static void main(String[] args) {
-        System.out.println("112233");
-    }
-}
