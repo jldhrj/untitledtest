4e08eb6a18301043d28acbafe4016a583e0ad1cc
jldhrj <jldhrj@163.com>
2023-10-12 18:12:13 +0800

    10122023

--- src/com/test/out/App20231011.java
+++ src/com/test/out/App20231011.java
@@ -5,6 +5,16 @@ public class App20231011 {
         System.out.println("123");
         System.out.println("123");
         System.out.println("123");
+        int aa = 18;
+        System.out.println(aa);
+        aa = aa + 18;
+        System.out.println(aa);
+        double wx = 0;
+        System.out.println(wx);
+        wx = wx + 9.4;
+        System.out.println(wx);
+        wx = wx - 5;
+        System.out.println(wx);
     }
 
 }
