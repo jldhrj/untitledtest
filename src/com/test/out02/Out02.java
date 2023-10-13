fca66d2a58e5f43d573297c788562b7180e0a207
jldhrj <jldhrj@163.com>
2023-10-13 18:04:42 +0800

    10132023

--- src/com/test/out02/Out02.java
+++ src/com/test/out02/Out02.java
@@ -2,6 +2,23 @@ package com.test.out02;
 
 public class Out02 {
     public static void main(String[] args) {
+        int a = 5;
         System.out.println("123");
+        System.out.println("aa" + a);
+        System.out.println(6 + a);
+        System.out.println("inini" + a + 'a');
+        System.out.println(a + 'a' + "ninin");
+        int b = 4;
+        --b;
+        System.out.println(b);
+        int c = 4;
+        int w = ++c ;
+        System.out.println(c);
+        System.out.println(w);
+        int aa=4;
+        int ee= aa++;
+        System.out.println(aa);
+        System.out.println(ee);
+
     }
 }
