3eb62bd3e852a5486965dc537873f87a3560fbf2
brad <jldhrj@gmail.com>
2023-10-19 23:24:33 +0800

    10192023

---
+++ src/com/test/loop/DoWhileDemo.java
@@ -0,0 +1,14 @@
+package com.test.loop;
+
+public class DoWhileDemo {
+    public static void main(String[] args) {
+        int i=0;
+        int b=1;
+        do {
+            System.out.println(b);
+            i++;
+            b++;
+        }while (i<5f);//false and true
+
+    }
+}
--- src/com/test/loop/WhileDemo.java
+++ src/com/test/loop/WhileDemo.java
@@ -2,6 +2,21 @@ package com.test.loop;
 
 public class WhileDemo {
     public static void main(String[] args) {
+        int i=0;
+        while(i<5){
+            System.out.println("asd"+i);
+            i++;
+        }
+
+        double zu=8848860;
+        double zi=0.1;
+        int nub=0;
+        while (zi<=zu){
+            nub++;
+            zi=zi*2;
+        }
+        System.out.println(nub);
+        System.out.println(zi);
 
     }
 }
