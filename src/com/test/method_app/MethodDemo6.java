902bb3f75091730527416b6442b8e0f44b5af9c4
brad <jldhrj@gmail.com>
2023-11-01 00:09:54 +0800

    11012023

--- src/com/test/method_app/MethodDemo6.java
+++ src/com/test/method_app/MethodDemo6.java
@@ -2,25 +2,27 @@ package com.test.method_app;
 
 public class MethodDemo6 {
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 2, 4};
+        int[] arr1 = {1, 2, 3, 4};
         int[] arr2 = {1, 2, 3, 4};
         System.out.println(toto(arr1, arr2));
     }
 
     public static boolean toto(int[] a, int[] b) {
-        boolean tf = false;
         if (a.length != b.length) {
-            tf = false;
-        }
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == b[i]) {
-                tf = true;
-            } else {
-                tf = false;
-                break;
+            return false;
+
+        } else if (a == null && b == null) {
+            return false;
+        } else if (a == null || b == null) {
+            return false;
+        } else {
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] != b[i]) {
+                    return false;
+                }
             }
         }
-
-        return tf;
+        return true;
     }
 }
+
