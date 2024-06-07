2c6ba36b395e7051a312581401d3a05d01acbdfe
jldhrj <jldhrj@163.com>
2024-06-07 16:40:19 +0800

    06072024

---
+++ src/com/test/string1/Stringdemo.java
@@ -0,0 +1,12 @@
+package com.test.string1;
+
+public class Stringdemo {
+    public static void main(String[] args) {
+        String name ="test";
+        System.out.println(name);
+        String rs1 = new String();
+        System.out.println(rs1);
+        String rs2 = new String("adfadsf");
+        System.out.println(rs2);
+    }
+}
--- src/com/test/string1/Stringdemo1.java
+++
@@ -1,8 +0,0 @@
-package com.test.string1;
-
-public class Stringdemo1 {
-    public static void main(Stringdemo1[] args) {
-        String name = "jjjj";
-        System.out.println(name);
-    }
-}
