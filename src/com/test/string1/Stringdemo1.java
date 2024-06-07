cc035548a7b860a099db9231802d6751c19f4c64
jldhrj <jldhrj@163.com>
2024-06-07 15:49:44 +0800

    06072024

---
+++ src/com/test/string1/Stringdemo1.java
@@ -0,0 +1,8 @@
+package com.test.string1;
+
+public class Stringdemo1 {
+    public static void main(Stringdemo1[] args) {
+        String name = "jjjj";
+        System.out.println(name);
+    }
+}
